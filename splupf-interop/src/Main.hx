4ff87e80557072c5e63b8f447a7d151551a6c890
drflamemontgomery <drflamemontgomery@gmail.com>
2023-02-15 17:40:38 +1300

    Updated Main to reflect the backend.\nCreated a new test string

--- splupf-interop/src/Main.hx
+++ splupf-interop/src/Main.hx
@@ -3,11 +3,9 @@ package;
 import parser.LanguageParser;
 
 class Main {
- 
 
-  // helper function for converting the data into a string 
-  static function splufpDataToString(data:SplufpDataType) : String {
-    switch(data) {
+  static function splufpExprToString(expr:SplufpExpr) {
+    switch(expr) {
       case SNull:
         return 'Null';
 
@@ -19,31 +17,41 @@ class Main {
 
       case SNumber(val):
         return 'Number $val';
-
+      case SVariable(val):
+        return 'Variable $val';
       case SArray(arr):
         var s = '[ ';
         for(i in arr) {
-          s += splufpDataToString(i) + ', ';
+          s += splufpExprToString(i) + ', ';
         }
 
         // \x08 is a backspace
-        s += '\x08\x08 ]';
+        // if we have an array of length 0 we don't want to erase it 
+        s += (arr.length > 0 ? '\x08\x08' : '\x08') + ' ]';
         return s;
+      case SObject(val):
+        var s = '{ ';
+          for(key in val.keys()) {
+            s += '${key} : ${splufpExprToString(val[key])}, ';
+          }
+          // \x08 is a backspace
+          s += '\x08\x08 }';
+        return s;
+    }
+    return ''; 
+  }
+
+  // helper function for converting the data into a string 
+  static function splufpDataToString(data:SplufpDataType) : String {
+    switch(data) {
+      case SVariable(const, name, expr):
+        return (const ? 'let' : 'set') + ' ${name} ${splufpExprToString(expr)}';
 
       case SFunc(const, name, args, body):
         return (const ? 'const ' : '') + 'function ${name} ${args} { ${body} }';
 
       case SJSFunc(name, args):
         return 'jsfunction ${name} ${args}';
-
-      case SObject(val):
-        var s = '{ ';
-        for(key in val.keys()) {
-          s += '${splufpDataToString(key)} : ${splufpDataToString(val[key])}, ';
-        }
-        // \x08 is a backspace
-        s += '\x08\x08 }';
-        return s;
 
       case SMacro(_, _, _):
         return 'Macro unsupported';
@@ -54,13 +62,58 @@ class Main {
   }
 
   static function printSplufpData(data:SplufpDataType) {
-    trace(splufpDataToString(data));
+#if js
+    js.Browser.console.log(splufpDataToString(data));
+#else
+    Std.printLn(splufpDataToString(data));
+#end
+
     return ''; // You need a return for Lambda.map
   }
 
   public static function main() {
     // print every element returned from the parsed text
-    Lambda.map(LanguageParser.parse("let asd a = 2"), printSplufpData);
+    final test_parsing = "
+let a = [ // Single Line Comment Test
+/**
+ *
+ * Multiline comment test
+ *
+ *
+**/
+  null,
+  true,
+  false,
+  'string\n\n\\\'string\\\'',
+  123,
+  -123,
+  1.3,
+  -1.2,
+  [ 1, 2, 3]
+]\n
+set b = [
+  [ 
+    [
+      1, '2', \"3\", null
+    ],
+
+    [
+      true, false, arg
+    ],
+
+    []
+
+  ]
+]
+  ";
+#if js
+    js.Browser.console.log('Parsing String:\n${test_parsing}');
+    js.Browser.console.log('Output:\n');
+#else
+    Std.printLn('Parsing String:\n${test_parsing}');
+    Std.printLn('Output:\n');
+#end
+    Lambda.map(LanguageParser.parse(test_parsing + '\n'), printSplufpData);
   }
 
 }
