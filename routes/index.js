eb4b76937da1be81fe0babec0da144ab2bd4bc83
drflamemontgomery <drflamemontgomery@gmail.com>
2023-02-23 18:29:38 +1300

    Started adding color schemes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+function nav_data(name, href) {
+  return {
+    name: name,
+    href, href
+  };
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var pass_through_data = { title: 'Express', links:
+    [
+      nav_data('Navbar1', '/Navbar1')
+    ]
+  };
+  console.log(pass_through_data);
+  res.render('index', pass_through_data);
+  next();
 });
 
 module.exports = router;
