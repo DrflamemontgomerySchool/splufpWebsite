76517b62d3d5488189629389cc64a0566ca4691e
drflamemontgomery <drflamemontgomery@gmail.com>
2023-03-10 17:47:58 +1300

    Updated Splufp

--- public/splufp/splufp-lang.js
+++ public/splufp/splufp-lang.js
@@ -1,6 +1,12 @@
 class __splufp__function {
   #value;
   constructor(value) {
+    if(typeof(value) == 'function') {
+      if(value.length > 0) {
+        this.value = function() { return value };
+        return;
+      }
+    }
     this.value = value;
   }
 
@@ -15,6 +21,12 @@ class __splufp__function {
 class __splufp__function_assignable {
   #value;
   constructor(value) {
+    if(typeof(value) == 'function') {
+      if(value.length > 0) {
+        this.value = function() { return value };
+        return;
+      }
+    }
     this.value = value;
   }
 
@@ -28,49 +40,6 @@ class __splufp__function_assignable {
   set_value(value) {
     this.value = value;
   }
-}
-
-
-function add(a, b) {
-  return a + b;
-}
-
-function sub(a, b) {
-  return a - b;
-}
-
-function mul(a, b) {
-  return a * b;
-}
-
-function div(a, b) {
-  return a / b;
-}
-
-function neg(a) {
-  return -a;
-}
-
-function obj_get(obj, name) {
-  return obj[name];
-}
-
-function array_at(arr, n) {
-  return arr[n];
-}
-
-function slice(arr, pos, end) {
-  return arr.slice(pos, end);
-}
-
-function splice(arr, pos, len) {
-  return arr.splice(pos, len);
-}
-
-
-
-function len(arr) {
-  return arr.length;
 }
 
 function foldr(fn, init, arr) {
@@ -99,56 +68,3 @@ function map(fn, arr) {
     map(fn, arr.slice(1, arr.length));
   }
 }
-
-function log(str) {
-  console.log(str);
-}
-
-
-function none() {
-  return;
-}
-
-function and(a, b) {
-  return a && b;
-}
-
-function or(a, b) {
-  return a || b;
-}
-
-function eq(a, b) {
-  return a == b;
-}
-
-function geq(a, b) {
-  return a >= b;
-}
-
-function leq(a, b) {
-  return a <= b;
-}
-
-function gt(a, b) {
-  return a > b;
-}
-
-function lt(a, b) {
-  return a < b;
-}
-
-
-
-/*var __spl__if = new __splufp__function(
-  function(statement) {
-    return function(if_fn) {
-      return function(else_fn) {
-        if(statement.call()) {
-          return if_fn.call()();
-        } else {
-          return else_fn.call()();
-        }
-      }
-    }
-  }
-);*/
